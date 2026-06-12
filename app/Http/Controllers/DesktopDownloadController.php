7e522d5b702c9958fdb05a96e6f3866811d4b546
Jawadislam922 <jawadislam92@gmail.com>
2026-06-13 03:51:38 +0500

    Desktop v0.3.0: SA Track name + new logo icon, downloads page updated

--- app/Http/Controllers/DesktopDownloadController.php
+++ app/Http/Controllers/DesktopDownloadController.php
@@ -8,9 +8,9 @@ use Inertia\Inertia;
 
 class DesktopDownloadController extends Controller
 {
-    private const WINDOWS_INSTALLER = 'Timetracker Desktop Setup 0.2.5.exe';
+    private const WINDOWS_INSTALLER = 'SA Track Setup 0.3.0.exe';
 
-    private const WINDOWS_VERSION = '0.2.5';
+    private const WINDOWS_VERSION = '0.3.0';
 
     private const MAC_VERSION = '0.1.0';
 
